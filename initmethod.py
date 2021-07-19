3c453b3c841e77f0dae375e622ae1e1d8a0b10d9
ludenyi <86718951+ludenyi@users.noreply.github.com>
2021-07-19 17:26:22 +0300

    Create initmethod.py

---
+++ initmethod.py
@@ -0,0 +1,12 @@
+#a sample class with init method
+class person:
+
+    #init method or constructor
+    def_init_(self,name):
+
+    #sample method
+    def say_hi(self):
+        print("hello,my name is", self.name)
+
+    p= person("Sharon")
+    p.say_hi()
