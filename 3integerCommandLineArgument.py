2324170ff35342f5efe529f1e6f8ef7d5a3d1fa1
ludenyi <86718951+ludenyi@users.noreply.github.com>
2021-07-19 17:27:03 +0300

    Create 3integerCommandLineArgument.py

---
+++ 3integerCommandLineArgument.py
@@ -0,0 +1,7 @@
+num1=int(input("please enter first number:"))
+num2=int(input("please enter second number:"))
+num3=int(input("please enter third number:"))
+if num1==(num2==num3):
+        print("equal")
+else:
+        print("not equal")
