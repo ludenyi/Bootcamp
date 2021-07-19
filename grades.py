11178e54d38c9ea1d172bf5a046b2f1c8606c0ac
ludenyi <86718951+ludenyi@users.noreply.github.com>
2021-07-19 17:26:30 +0300

    Create grades.py

---
+++ grades.py
@@ -0,0 +1,25 @@
+num = int(input("please enter your mark:\n"))
+if num<=100 and num>=79:
+    print("you are a genious you got an A")
+elif num<=79 and num>=75:
+    print("you got an A-")
+elif num<=74 and num>=70:
+    print("you got a B+")
+elif num<=69 and num>=65:
+    print("you got a B")
+elif num<=64 and num>+60:
+    print("you got a B-")
+elif num<=59 and num>=55:
+    print("you got a C+")
+elif num<=54 and num>=45:
+    print("you got a C")
+elif num<=44 and num>=40:
+    print("you got a C-")
+elif num<=39 and num>=35:
+    print("you got a D+")
+elif num<=34 and num>=30:
+    print("you got a D")
+elif num<=29 and nu>=25:
+    print("you got a D-")
+elif num<=below25:
+    print("you failed")
