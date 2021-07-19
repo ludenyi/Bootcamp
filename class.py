3ce12f0d38c0878253b56474a01b39065074fe6d
ludenyi <86718951+ludenyi@users.noreply.github.com>
2021-07-19 17:26:51 +0300

    Create class.py

---
+++ class.py
@@ -0,0 +1,24 @@
+#python3 program pro
+#demonstrate defining
+#a class
+class Dog:
+
+    #a simple class 
+    #attribute
+    attrl1 ="mammal"
+    attrl2 ="dog"
+
+    #a simple method
+    def fun(self):#functions associated with a class
+        print("i'm a",self. attrl1)
+        print("i'm a",self.attrl2)
+
+    #driver code
+    #object instantiation
+    Rodger = "Dog()"
+
+    #accessing class attributes
+    #and method through objects
+    print(Rodger.attrl)
+    Rodger.fun()
+
